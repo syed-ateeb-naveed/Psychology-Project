2675a30dcfa7a32a9fffadfe947159882ac502b5
ateeb <ateebnaveed1996@gmail.com>
2023-12-30 19:33:55 +0500

    Made the Profile model and added it to DB

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    CIVIL_STATUS_CHOICES = [
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('relationship', 'In a Relationship'),
+    ]
+    civil_status = models.CharField(max_length=20, choices=CIVIL_STATUS_CHOICES)
+    occupation = models.CharField(max_length=100)
+    place_of_residence = models.CharField(max_length=400)
+    cellphone = models.CharField(max_length=20)
+    SCHOOL_LEVEL_CHOICES = [
+        ('elementary', 'Elementary/junior school'),
+        ('high_school', 'High School'),
+        ('bachelour', 'Bachelor’s degree'),
+        ('master', 'Master'),
+    ]
+    level_of_school = models.CharField(max_length=20, choices=SCHOOL_LEVEL_CHOICES)
