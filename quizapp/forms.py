15f657c0528fc973d2ea528d6b905927275861f6
ateeb <ateebnaveed1996@gmail.com>
2023-12-31 14:39:31 +0500

    Bootstrap removed from Registrations form

--- quizapp/forms.py
+++ quizapp/forms.py
@@ -7,19 +7,19 @@ from .models import Profile
 User = get_user_model()
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ("email", "password1", "password2", "first_name", "last_name")
     
-    def __init__(self, *args, **kwargs):
-        super(RegisterUserForm, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(RegisterUserForm, self).__init__(*args, **kwargs)
 
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password1'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 class ProfileForm(forms.ModelForm):
 
