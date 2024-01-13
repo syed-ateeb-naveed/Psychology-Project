99237c337e860f063e8f29a62342c0e615afe59c
ateeb <ateebnaveed1996@gmail.com>
2024-01-13 14:51:39 +0500

    Fixed login page backend

--- quizapp/forms.py
+++ quizapp/forms.py
@@ -7,8 +7,10 @@ from .models import Profile
 User = get_user_model()
 
 class RegisterUserForm(UserCreationForm):
-    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    # first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
