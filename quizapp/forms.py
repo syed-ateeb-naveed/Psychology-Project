263cc4b25a48b85c6c25a9a28f1a33ecb1a72a47
ateeb <ateebnaveed1996@gmail.com>
2024-01-13 15:13:04 +0500

    Fixed Registration form's frontend

--- quizapp/forms.py
+++ quizapp/forms.py
@@ -7,21 +7,21 @@ from .models import Profile
 User = get_user_model()
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # password1 = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # password2 = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     # last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ("email", "password1", "password2", "first_name", "last_name")
     
-    # def __init__(self, *args, **kwargs):
-    #     super(RegisterUserForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    #     self.fields['password1'].widget.attrs['class'] = 'form-control'
-    #     self.fields['password2'].widget.attrs['class'] = 'form-control'
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
 
 class ProfileForm(forms.ModelForm):
 
@@ -32,3 +32,9 @@ class ProfileForm(forms.ModelForm):
             'civil_status': forms.Select(choices=Profile.CIVIL_STATUS_CHOICES),
             'level_of_school': forms.Select(choices=Profile.SCHOOL_LEVEL_CHOICES),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
